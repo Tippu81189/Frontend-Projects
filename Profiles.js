c8b4d4f2c7a3ffcad9ad002a46394373bd3ac889
Tippu Shaik <97241987+Tippu81189@users.noreply.github.com>
2023-05-25 17:03:27 +0530

    Profiles Card

---
+++ Profiles.js
@@ -0,0 +1,3 @@
+//<a href="https://imgbb.com/"><img src="https://i.ibb.co/Wc2k3M2/IMG-20230215-WA0005.jpg" alt="IMG-20230215-WA0005" border="0"></a>
+//<a href="https://ibb.co/5vNTrrb"><img src="https://i.ibb.co/Np8x99c/IMG-2348.jpg" alt="IMG-2348" border="0"></a>
+//<a href="https://ibb.co/5s1dNjX"><img src="https://i.ibb.co/0Ynkg9p/SNR0471.jpg" alt="SNR0471" border="0"></a>
